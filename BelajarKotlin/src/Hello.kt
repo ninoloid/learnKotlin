26c7986645edb29f1017f93db32c2b1778067228
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-04-23 22:27:00 +0700

    upload

---
+++ BelajarKotlin/src/Hello.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+}
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
+
+fun main(args: Array<String>) {
+    feedTheFish()
+}
